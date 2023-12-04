ba8a7c31fb03d110f39c407efdc70cffa27d3a42
Andrii Dobronos <andriidobronos@gmail.com>
2023-12-04 16:52:12 +0200

    homework8

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,98 @@
-//Написати функцію convertToString, яка може приймати аргумент будь якого типу
-// та повертати його у вигляді строки (за допомогою методу toString).
-// Якщо цього методу немає, тоді повертати undefined.
+//Вам потрібно створити тип DeepReadonly який буде робити
+// доступними тільки для читання навіть властивості
+// вкладених обʼєктів.
 
-// Написати функцію sortEntities, яка має сортувати різні об'єкти, які мають id
-// за зростанням або за зменшенням. Функція приймає 2 аргументи - масив об'єктів
-// та ключове слово desc або asс, відповідно до нього буде відбуватися сортування.
-
-function convertToString<T>(arg:T):string | undefined {
-    if (arg &&  typeof (arg as any).toString === "function") {
-        return (arg as any).toString()
-    }
-    return
-}
-console.log(convertToString<number[]>([34,35]))
-console.log(convertToString<boolean[]>([true,false]))
-
-//******************************************************//
-
-interface IEntities {
-    id:number ;
+type DeepReadonly<T> = {
+    readonly [P in keyof T]:T[P] extends object ? DeepReadonly<T[P]> : T[P]
 }
 
-type TKeyWord = "desc" | "asc"
-
-function sortEntities<T extends IEntities>(arg :T[], keyWord:TKeyWord):T[] {
-    return arg.sort((a:T, b:T) => {
-        if (keyWord === "asc") {
-            return a.id - b.id;
-        } else {
-            return b.id - a.id;
-        }
-    })
+interface IExample1 {
+    mutable: string;
+    nested: {
+        mutableNested: number;
+    };
 }
 
-console.log(sortEntities<any>([{id:2},{id:5},{id:1}],"desc"))
-console.log(sortEntities<any>([{id:2},{id:5},{id:1}],"asc"))
+const example1: DeepReadonly<IExample1> = {
+    mutable: 'readonly',
+    nested: {
+        mutableNested: 7,
+    },
+};
+
+//********************************************************//
+
+//Вам потрібно створити тип DeepRequireReadonly який буде
+// робити доступними тільки для читання навіть властивості
+// вкладених обʼєктів та ще й робити їх обовʼязковими.
+
+type DeepRequireReadonly<T> = {
+    readonly [P in keyof T]-?:T[P] extends object ? DeepReadonly<T[P]> : T[P]
+}
+
+interface Example2 {
+    mutable?: string;
+    nested?: {
+        mutableNested?: number;
+    };
+}
+
+const example2: DeepRequireReadonly<Example2> = {
+    mutable: 'readonly',
+    nested: {
+        mutableNested: 7,
+    },
+};
+
+//**************************************************************//
+
+//Вам потрібно сворити тип UpperCaseKeys, який буде приводити
+//всі ключи до верхнього регістру.
+
+type UpperCaseKeys<T> = {
+     // [P in keyof T as Uppercase<P>]: T[P]
+     [P in keyof T as Uppercase<string & P>]:T[P]
+    // [P in keyof T as Uppercase<Extract<`${P}`, string>>]: T[P]
+}
+
+interface IExample3 {
+    name: string;
+    age: number;
+}
+
+const example3:UpperCaseKeys<IExample3> = {
+    NAME:"Bird",
+    AGE: 3
+}
+
+//*********************************************************//
+
+//Вам потрібно зробити свій аналог утіліти Pick, яка конструює новий тип,
+// який буде включати в себе лише параметри передані в цю утіліту.
+// Наприклад:
+// interface User {
+//   name: string;
+//   age: number;
+//   permission: string[];
+// }
+//
+//
+// let newUser: TPick<User, "name" | "age">;
+//повинен створити новий тип, який має включати в себе лише проперті
+// name та age, без permissions
+
+type  TPick<T,U extends keyof T> = {
+    [P in U]: T[P];
+}
+
+interface User {
+  name: string;
+  age: number;
+  permission: string[];
+}
+
+let newUser: TPick<User, "name" | "age"> = {
+    name:"",     //ok
+    age:1,      // ok
+//  permission:[" "," "] // error
+}
