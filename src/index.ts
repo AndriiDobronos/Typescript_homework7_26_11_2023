438778cb7f0ba83a954f2380031ab429cdbda531
Andrii Dobronos <andriidobronos@gmail.com>
2023-12-06 16:01:22 +0200

    homework9

--- src/index.ts
+++ src/index.ts
@@ -1,98 +1,40 @@
-//Вам потрібно створити тип DeepReadonly який буде робити
-// доступними тільки для читання навіть властивості
-// вкладених обʼєктів.
+//Вам потрібно створити умовний тип, що служить для встановлення типу,
+//що повертається з функції. Як параметр типу повинен обов'язково
+// виступати функціональний тип.
 
-type DeepReadonly<T> = {
-    readonly [P in keyof T]:T[P] extends object ? DeepReadonly<T[P]> : T[P]
+type ConditionalTypeReturnFromFunc<T> = T extends (...param:infer P) =>(infer U) ? U
+    : "the condition is not met"
+
+function funcExample(numeric: number): boolean {
+    return Boolean(numeric)
 }
 
-interface IExample1 {
-    mutable: string;
-    nested: {
-        mutableNested: number;
-    };
+function funcExample1(numeric: number) {
+    return
 }
 
-const example1: DeepReadonly<IExample1> = {
-    mutable: 'readonly',
-    nested: {
-        mutableNested: 7,
-    },
-};
+type ReturnFromFuncType = ConditionalTypeReturnFromFunc<typeof funcExample> // boolean
+type ReturnFromFuncType1 = ConditionalTypeReturnFromFunc<typeof funcExample1> // void
+type ReturnFromFuncType2 = ConditionalTypeReturnFromFunc<typeof funcExample4> // "the condition is not met"
 
-//********************************************************//
+//********************************************************************//
 
-//Вам потрібно створити тип DeepRequireReadonly який буде
-// робити доступними тільки для читання навіть властивості
-// вкладених обʼєктів та ще й робити їх обовʼязковими.
+//Вам потрібно створити умовний тип, який приймає функціональний тип
+//з одним параметром (або задовільним) та повертає кортеж, де перше
+//значення - це тип, що функція повертає, а другий - тип її параметру.
 
-type DeepRequireReadonly<T> = {
-    readonly [P in keyof T]-?:T[P] extends object ? DeepReadonly<T[P]> : T[P]
+type ConditionalTypeReturnCortege<T> = T extends (...param:infer P) =>(infer U) ? [U,P]
+    : "the condition is not met"
+
+function funcExample2(numeric: number): boolean {
+    return Boolean(numeric)
 }
 
-interface Example2 {
-    mutable?: string;
-    nested?: {
-        mutableNested?: number;
-    };
+function funcExample3(numeric: number) {
+    return
 }
 
-const example2: DeepRequireReadonly<Example2> = {
-    mutable: 'readonly',
-    nested: {
-        mutableNested: 7,
-    },
-};
-
-//**************************************************************//
-
-//Вам потрібно сворити тип UpperCaseKeys, який буде приводити
-//всі ключи до верхнього регістру.
-
-type UpperCaseKeys<T> = {
-     // [P in keyof T as Uppercase<P>]: T[P]
-     [P in keyof T as Uppercase<string & P>]:T[P]
-    // [P in keyof T as Uppercase<Extract<`${P}`, string>>]: T[P]
-}
-
-interface IExample3 {
-    name: string;
-    age: number;
-}
-
-const example3:UpperCaseKeys<IExample3> = {
-    NAME:"Bird",
-    AGE: 3
-}
-
-//*********************************************************//
-
-//Вам потрібно зробити свій аналог утіліти Pick, яка конструює новий тип,
-// який буде включати в себе лише параметри передані в цю утіліту.
-// Наприклад:
-// interface User {
-//   name: string;
-//   age: number;
-//   permission: string[];
-// }
-//
-//
-// let newUser: TPick<User, "name" | "age">;
-//повинен створити новий тип, який має включати в себе лише проперті
-// name та age, без permissions
-
-type  TPick<T,U extends keyof T> = {
-    [P in U]: T[P];
-}
-
-interface User {
-  name: string;
-  age: number;
-  permission: string[];
-}
-
-let newUser: TPick<User, "name" | "age"> = {
-    name:"",     //ok
-    age:1,      // ok
-//  permission:[" "," "] // error
-}
+let funcExample4 = 4
+type ReturnResultType2 =  ConditionalTypeReturnCortege<typeof funcExample2> // [boolean, [number]]
+type ReturnResultType3 =  ConditionalTypeReturnCortege<typeof funcExample3> // [void, [number]]
+type ReturnResultType4 =  ConditionalTypeReturnCortege<typeof funcExample4> // "the condition is not met"
